dcc13e7a90f8a8d2c3c4a09915d08a89429fc5e1
LucasJavierBarros <lucasbarros78@hotmail.com>
2022-09-24 23:14:09 -0300

    actors finalizado

--- src/controllers/actorsController.js
+++ src/controllers/actorsController.js
@@ -4,23 +4,17 @@ module.exports = {
     list : (req,res) => {
         db.Actor.findAll()
             .then((actors) => {
-                return res.render('genresList',{
+                return res.render('actorsList',{
                     actors
                 })
             })
             .catch(error => console.log(error))
     },
-    new : (req,res) => {
-
-    },
-    recomended : (req,res) => {
-
-    },
     detail : (req,res) => {
-        db.Genre.findByPk(req.params.id)
-        .then((genre) => {
-            return res.render("genresDetail",{
-                genre
+        db.Actor.findByPk(req.params.id)
+        .then((actor) => {
+            return res.render("actorsDetail",{
+                actor
             })
         })
         .catch((error) => console.log(error)); 
